ab99f586615e8cbb337f7ebbfe9575c7c8bfb240
mikitaro-27 <matt.diak@gmail.com>
2022-03-07 19:20:02 +0000

    Change taskModel to incorporate endDate

--- jobs/refreshCompletedReps.js
+++ jobs/refreshCompletedReps.js
@@ -2,8 +2,11 @@ const schedule = require('node-schedule')
 const Task = require("../models/taskModel");
 
 const refreshCompletedReps = async () => {
-  schedule.scheduleJob('*/5 * * * * *', async () => {
+  schedule.scheduleJob('*/10 * * * * *', async () => {
+    // get all tasks completed reps
+    // add them to history array
     await Task.updateMany({}, {completedReps: 0})
+
     console.log(`Job completed: cleared tasks completedReps`.magenta.underline)
   })
 }
